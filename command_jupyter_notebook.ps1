dd0defdbc6f93e93020d53e7d87c784e5ff86bc5
Evib21 <150394187+Evib21@users.noreply.github.com>
2025-05-26 23:19:21 -0400

    update W

---
+++ command_jupyter_notebook.ps1
@@ -0,0 +1,6 @@
+# Run this in power shell to open jupyter notebook in browser
+& "$env:USERPROFILE\Anaconda3\Scripts\jupyter.exe" notebook --notebook-dir="C:\Users\Evi_Beba\IT\Python\Python"
+
+# Using the above as a function, and calling that shortcut:
+. $PROFILE 
+jn 
